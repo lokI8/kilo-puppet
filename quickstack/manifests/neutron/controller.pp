bbfa35b91eb5ec1682694857428b1ab8909fe680
root <root@foreman.moc.ne.edu>
2015-09-10 18:03:20 +0000

    changed port for neutron_metadata auth to 5000 from 35357

--- quickstack/manifests/neutron/controller.pp
+++ quickstack/manifests/neutron/controller.pp
@@ -430,7 +430,7 @@ class quickstack::neutron::controller (
   class { 'neutron::agents::metadata':
     auth_password => $neutron_user_password,
     shared_secret => $neutron_metadata_proxy_secret,
-    auth_url      => "http://${controller_priv_host}:35357/v2.0",
+    auth_url      => "http://${controller_priv_host}:5000/v2.0",
     metadata_ip   => $controller_priv_host,
   }
 
