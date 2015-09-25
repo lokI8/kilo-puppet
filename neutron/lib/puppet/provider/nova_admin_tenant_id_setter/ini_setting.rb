75cce8c53568072d2676add2a42113ca64c6308b
Rahul <rahulsharmaait@gmail.com>
2015-09-25 03:34:11 -0400

    Fix neutron-configuration while pupetizing.
    Added fix to allow self-signed certificates. Earlier, it
    used to fail if the certificate is self signed, now it works
    properly as its ignored. This is a temporary fix, we need a proper
    fix where we pass the ca_file.

--- neutron/lib/puppet/provider/nova_admin_tenant_id_setter/ini_setting.rb
+++ neutron/lib/puppet/provider/nova_admin_tenant_id_setter/ini_setting.rb
@@ -12,6 +12,7 @@ require 'rubygems'
 require 'net/http'
 require 'net/https'
 require 'json'
+require 'openssl'
 require 'puppet/util/inifile'
 
 class KeystoneError < Puppet::Error
@@ -38,7 +39,14 @@ def neutron_handle_request(req, url)
         # found that if we use Net::HTTP.start with url.hostname the incriminated code
         # won't be hit.
         use_ssl = url.scheme == "https" ? true : false
-        http = Net::HTTP.start(url.hostname, url.port, {:use_ssl => use_ssl})
+	# Temporary fix to ignore verification of self-signed certs. If nova endpoint is
+	# configured to use https, it used to fail if certificate is self-signed. Ignoring
+	# it for now, need to handle it by passing ca_cert in future.
+	if use_ssl
+	    http = Net::HTTP.start(url.hostname, url.port, {:use_ssl => use_ssl, :verify_mode => OpenSSL::SSL::VERIFY_NONE})
+	else
+	    http = Net::HTTP.start(url.hostname, url.port, {:use_ssl => use_ssl})
+	end
         res = http.request(req)
 
         if res.code != '200'
