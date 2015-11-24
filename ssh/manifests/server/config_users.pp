60550e212728de524ca0c59b39ccfad45de54353
Rahul Sharma <rahulsharmaait@gmail.com>
2015-11-23 19:04:53 -0500

    Updated code with comments, removed extra exec.

--- ssh/manifests/server/config_users.pp
+++ ssh/manifests/server/config_users.pp
@@ -1,11 +1,33 @@
+# This file implements logic to add and configure users.
+#
+# $users: is an array consisting of users with their username,
+#         password and hashed_password value.
+#
+# Input is in the form:
+# [
+#   {"username": "user1",
+#    "password": "pass1",
+#    "hashed_password": <hashed_password_value>,
+#   },
+#   {"username": "user2",
+#    "password": "pass2",
+#    "hashed_password": <hashed_password_value>,
+#   },
+#   ...
+# ]
+#
+# To generate hashed_password, one can use below mentioned
+# python command:
+# python3 -c 'import crypt; print(crypt.crypt("password", crypt.mksalt()))'
+#
 class ssh::server::config_users (
   $users = $quickstack::params::moc_users
 ) {
 
-  define config_user ($user_config = $title) {
-    $user = $user_config['username']
-    $password = $user_config['password']
-    $hashed_password = $user_config['hashed_password']
+  define config_user ($config = $name) {
+    $user            = $config['username']
+    $password        = $config['password']
+    $hashed_password = $config['hashed_password']
 
     case $::osfamily {
       redhat: {
@@ -37,9 +59,7 @@ class ssh::server::config_users (
 	  group   => $user,
 	  mode    => '0400',
 	  replace => true,
-        } ->
-        exec {"store-passwd-${user}":
-          command => "/bin/echo ${password} > /home/${user}/access.txt",
+	  content => $password,
         }
       }
       default: {
